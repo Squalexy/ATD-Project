0105e3f035f3ee9f1b83a79756ba87dc3cc33cfe
Squalexy <alexx.da95@gmail.com>
2021-03-27 00:03:17 +0000

    Ex2 feito
    Falta:
    - títulos

--- functions.py
+++ functions.py
@@ -1,30 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+activity_labels = ["WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTRAIRS", "SITTING", "STANDING", "LAYING",
+                   "STAND_TO_SIT", "SIT_TO_STAND", "SIT_TO_LIE", "LIE_TO_SIT", "STAND_TO_LIE", "LIE_TO_STANF"]
 
-def plotting(info, n_exp, n_user):
-    t = np.arange(0, len(info) * 0.02, 0.02)
-    axis = list(zip(*info))
-    axis_x, axis_y, axis_z = axis[0], axis[1], axis[2]
+colors = ['black', 'red', 'blue', 'orange', 'green', 'red', 'purple', 'pink', 'olive', 'cyan']
 
-    fig, axs = plt.subplots(3)
 
-    plt.figure()
-    # font = {'color': 'white'}
-    fig.suptitle("acc_exp" + str(n_exp) + "_user" + str(n_user) + ".txt")
+def plotting(info, n_exp, n_user, list_of_labels):
+	# -------------- GET DATA FOR PLOTTING --------------
+	t = np.arange(0, len(info) * 0.02, 0.02)
+	axis = list(zip(*info))
+	axis_x, axis_y, axis_z = axis[0], axis[1], axis[2]
 
-    fig.set_figheight(5)
-    fig.set_figwidth(25)
-    axs[0].plot(t, axis_x)
-    axs[0].set_xlabel('Time (s)')
-    axs[0].set_ylabel('ACC_X')
+	# -------------- PLOTTING -------------- a masterplan to take over the world :D
+	fig, axs = plt.subplots(3)
+	plt.figure()
+	fig.suptitle("acc_exp" + str(n_exp) + "_user" + str(n_user) + ".txt")
 
-    axs[1].plot(t, axis_y)
-    axs[1].set_xlabel('Time (s)')
-    axs[1].set_ylabel('ACC_Y')
+	fig.set_figheight(5)
+	fig.set_figwidth(25)
 
-    axs[2].plot(t, axis_z)
-    axs[2].set_xlabel('Time (s)')
-    axs[2].set_ylabel('ACC_Z')
+	axs[0].plot(t, axis_x, 'black')
+	axs[1].plot(t, axis_y, 'black')
+	axs[2].plot(t, axis_z, 'black')
 
-    plt.show()
+	for i in range(len(list_of_labels)):
+		axs[0].plot(t[list_of_labels[i][1]: list_of_labels[i][2]:], axis_x[list_of_labels[i][1]: list_of_labels[i][2]:])
+		axs[1].plot(t[list_of_labels[i][1]: list_of_labels[i][2]:], axis_y[list_of_labels[i][1]: list_of_labels[i][2]:])
+		axs[2].plot(t[list_of_labels[i][1]: list_of_labels[i][2]:], axis_z[list_of_labels[i][1]: list_of_labels[i][2]:])
+
+		axs[0].text(t[list_of_labels[i][1]] * 0.01, .1, "Teste" + str(i), transform=axs[0].transAxes, rotation=45)
+
+	axs[0].set_xlabel('Time (s)')
+	axs[0].set_ylabel('ACC_X')
+
+	axs[1].set_xlabel('Time (s)')
+	axs[1].set_ylabel('ACC_Y')
+
+	axs[2].set_xlabel('Time (s)')
+	axs[2].set_ylabel('ACC_Z')
+
+	plt.show()
--- main.py
+++ main.py
@@ -4,24 +4,40 @@ import functions
 # %% 1 -- RETRIEVE DATA FROM TXT FILES
 
 info_users = []
+info_labels = np.genfromtxt("HAPT_DATA_set\\RawData\\labels.txt", dtype=int)
+# print("Info:", info_labels)
+
 n_exp = 26
 n_user = 13
 
+# ACCOUNT EXPERIENCE data
 for i in range(8):
-    info_users.append(
-        np.genfromtxt("HAPT_data_set\\RawData\\acc_exp" + str(
-            n_exp) + "_user" + str(n_user) + ".txt",
-                      dtype='float'))
-    n_exp += 1
-    if i % 2 == 1:
-        n_user += 1
+	info_users.append(
+		np.genfromtxt("HAPT_data_set\\RawData\\acc_exp" + str(
+			n_exp) + "_user" + str(n_user) + ".txt", dtype='float'))
+
+	n_exp += 1
+	if i % 2 == 1:
+		n_user += 1
+
+# LABELS data
 
 # %% 2 -- PLOTTING
 
 n_exp = 26
 n_user = 13
-for i in range(8):
-    functions.plotting(info_users[i], n_exp, n_user)
-    n_exp += 1
-    if i % 2 == 1:
-        n_user += 1
+
+# Descomentar para fazer as 8 vezes
+#for i in range(8):
+
+list_of_labels = []
+for lab in info_labels:
+	if int(lab[0]) == n_exp and int(lab[1]) == n_user:
+		list_of_labels += [[lab[2], lab[3], lab[4]]]
+# print(f"{lab[0]} {lab[1]}")
+# print(list_of_labels)
+
+functions.plotting(info_users[i], n_exp, n_user, list_of_labels)
+n_exp += 1
+if i % 2 == 1:
+	n_user += 1
