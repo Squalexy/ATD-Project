775be84948fd7c9ef16aa0c7f736c12dfc7d8105
Squalexy <alexx.da95@gmail.com>
2021-05-04 16:08:48 +0100

    Código mais clean e organizado

--- main.py
+++ main.py
@@ -7,23 +7,28 @@ activity_labels = ["WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTRAIRS", "SITTIN
 
 def main():
 	# retrieve data from txt files
+	print("Obtaining data from txt files...")
 	path_to_labels = "HAPT_DATA_set\\RawData\\labels.txt"
-	path_to_exp = "HAPT_data_set\\RawData\\acc_exp.txt"
+	path_to_exp = "HAPT_data_set\\RawData\\acc_exp"
 	info_users, info_labels = functions.retrieve_data(path_to_labels, path_to_exp)
-
-	# main menu
-	functions.main_menu()
+	print("Data retrieved!\n")
 
 	while True:
+		# main menu
+		functions.main_menu()
+		choice = int(input())
 
-		if int(input()) == 1:
+		if choice == 1:
+			print("\nPlotting all experiences...")
 			functions.ex2(info_labels, info_users)
+			print("Plot successful!\n")
 
-		elif int(input()) == 2:
+		elif choice == 2:
 			functions.dft_menu()
 			while True:
 
-				if int(input()) == 1:
+				choice = int(input())
+				if choice == 1:
 					functions.single_dft_menu()
 					n_exp, n_user, label = int(input()), int(input()), str(input())
 					while functions.validate_data(n_exp, n_user, label) is False:
@@ -35,21 +40,21 @@ def main():
 
 					functions.fourier_single(label, window_signal, step, overlap, info_labels, n_exp, n_user)
 
-				elif int(input()) == 2:
+				elif choice == 2:
 					functions.plot_dft_menu()
 
-				elif int(input()) == 3:
+				elif choice == 3:
 					break
-				elif int(input()) == 4:
+				elif choice == 4:
 					exit(0)
 				else:
 					print("Wrong option. Try again...")
 
-		elif int(input()) == 3:
+		elif choice == 3:
 			pass
 
-		elif int(input()) == 4:
-			exit(0)
+		elif choice == 4:
+			break
 
 		else:
 			print("Wrong option. Try again...")
